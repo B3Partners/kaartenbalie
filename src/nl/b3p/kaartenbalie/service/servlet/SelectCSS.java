d9af828374293ce277a375621f27bcc15f661614
Geert Plaisier <geert.plaisier@gmail.nl>
2007-04-19 13:19:44 +0000

    StyleSelect Kaartenbalie

--- src/nl/b3p/kaartenbalie/service/servlet/SelectCSS.java
+++ src/nl/b3p/kaartenbalie/service/servlet/SelectCSS.java
@@ -8,33 +8,25 @@ import javax.servlet.*;
 public class SelectCSS extends HttpServlet {
     public static final long serialVersionUID = 24362462L;
 
-    private String DOMAIN = "http://localhost:8084/kaartenbalie_mnp/styles/";
-    private String DEFAULT = "main";
+    private String STYLESFOLDER = "styles/";
+    private String DEFAULT = "b3p";
+    private String DOMAIN = "http://localhost:8084/kaartenbalie_mnp/";
     
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException {
         String s = "";
-        if(req.getParameter("subdomain").equals("")) {
-            StringBuffer url = req.getRequestURL();
-            int index1 = url.indexOf("//");
-            s = url.substring(index1 + 2, url.length());
-
-            int index2 = s.indexOf(":");
-            if(index2 == -1) index2 = s.indexOf("/");
-            s = s.substring(0, index2);
-
-            int index3 = s.indexOf(".");
-            if(index3 == -1) {
-                // Geen subdomain - Default stylesheet
-                s = DEFAULT;
-            } else {
-                // Select subdomain
-                s = s.substring(0, index3);
-            }
+        
+        if(req.getParameter("subdomain") == null) {
+            s = getStylesheetName(req.getRequestURL());
         } else {
-            s = req.getParameter("subdomain");
+            if(req.getParameter("subdomain").equals("")) {
+                s = getStylesheetName(req.getRequestURL());
+            } else {
+                s = req.getParameter("subdomain");
+            }
         }
         
-        String cssUrl = DOMAIN + s + ".css";
+        
+        String cssUrl = DOMAIN + STYLESFOLDER + s + ".css";
         try {
             URL cssur = new URL(cssUrl);
             StringBuffer result = new StringBuffer();
@@ -42,12 +34,15 @@ public class SelectCSS extends HttpServlet {
             try {
                 reader = new BufferedReader( new InputStreamReader(cssur.openStream()));
             } catch (FileNotFoundException e) {
-                cssUrl = DOMAIN + DEFAULT + ".css";
+                cssUrl = DOMAIN + STYLESFOLDER + DEFAULT + ".css";
                 cssur = new URL(cssUrl);
                 reader = new BufferedReader( new InputStreamReader(cssur.openStream()));
             }
             String line = null;
             while ( (line = reader.readLine()) != null) {
+                if(line.indexOf("url") != -1) {
+                    line = line.replace("../", DOMAIN);
+                }
                 result.append(line);
             }
             String content = result.toString();
@@ -62,5 +57,25 @@ public class SelectCSS extends HttpServlet {
         } catch(IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    private String getStylesheetName(StringBuffer url) {
+        String s = "";
+        int index1 = url.indexOf("//");
+        s = url.substring(index1 + 2, url.length());
+
+        int index2 = s.indexOf(":");
+        if(index2 == -1) index2 = s.indexOf("/");
+        s = s.substring(0, index2);
+
+        int index3 = s.indexOf(".");
+        if(index3 == -1) {
+            // Geen subdomain - Default stylesheet
+            s = DEFAULT;
+        } else {
+            // Select subdomain
+            s = s.substring(0, index3);
+        }
+        return s;
     }
 }
--- web/algemeen/main/hIndex.jsp
+++ web/algemeen/main/hIndex.jsp
@@ -1,3 +1,4 @@
 <%@include file="/templates/taglibs.jsp" %>
 
-<img src="<html:rewrite page='/images/siteImages/kop.jpg' module='' />" width="1000">
+<!-- <img src="<html:rewrite page='/images/siteImages/kop.jpg' module='' />" width="1000"> -->
+<div id="kopImage"></div>
