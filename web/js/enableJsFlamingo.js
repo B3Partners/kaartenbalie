25c2087b180f921f1c9fcd2d17c86e14ff02a41a
Nando de Goeij <nandodegoeij@gmail.com>
2007-10-15 09:30:28 +0000

    Test commit andere versie enableJsFlamingo.js toegevoegd.
    Former-commit-id: c60caa6b5715a3a1f2b7443db51e4f4d018a5d32

--- web/js/enableJsFlamingo.js
+++ web/js/enableJsFlamingo.js
@@ -1,13 +1,14 @@
 //always call this script after the SWF object script has called the flamingo viewer.
-var flamingo;
-function init() {
-   if (document.getElementById) {
-      flamingo = document.getElementById("flamingo");
-   }
-}
-window.onload = init;
+var flamingo = document.getElementById("flamingo");
+
 function flamingo_onInit() {
    //at this moment the flamingo.swf is up and running, so initialize the global flamingo var.
    flamingo =getMovie("flamingo");
 }
-
+function getMovie(movieName) {   
+   if (navigator.appName.indexOf("Microsoft") != -1) {
+            return window[movieName];
+   }else {
+      return document[movieName];
+   }
+}
