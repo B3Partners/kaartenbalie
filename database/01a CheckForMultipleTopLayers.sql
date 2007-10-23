bbe5346a7b928c9f4bc9e1563ecb43b32373f6db
Nando de Goeij <nandodegoeij@gmail.com>
2007-10-23 08:07:24 +0000

    Klein foutje in toevoegen.
    Former-commit-id: 9f0a116ef403c5dcde41d9dbc5a543f42c64ab3e

--- database/01a CheckForMultipleTopLayers.sql
+++ database/01a CheckForMultipleTopLayers.sql
@@ -1,4 +1,2 @@
 SELECT * FROM layer l WHERE l.serviceproviderid =
     (SELECT serviceproviderid FROM layer WHERE parentid IS NULL GROUP BY serviceproviderid HAVING COUNT(*) > 1)
-AND l.layerid not in 
-    (SELECT ol.layerid FROM organizationlayer ol WHERE ol.layerid = l.layerid)
