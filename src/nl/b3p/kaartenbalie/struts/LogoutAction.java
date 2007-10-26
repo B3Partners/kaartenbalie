8d5be27da66fdb6004dc2e11654a89ff8e8dd041
Chris van Lith <chrisvanlith@gmail.com>
2007-10-26 08:20:41 +0000

    extra info
    Former-commit-id: c527e73feeeec94bf62689ba60c540d4552daa0e

--- src/nl/b3p/kaartenbalie/struts/LogoutAction.java
+++ src/nl/b3p/kaartenbalie/struts/LogoutAction.java
@@ -6,6 +6,9 @@ package nl.b3p.kaartenbalie.struts;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.*;
 
 /**
@@ -13,10 +16,16 @@ import org.apache.struts.action.*;
  */
 public class LogoutAction extends Action {
 
+    private static final Log log = LogFactory.getLog(LogoutAction.class);
+    
     public ActionForward execute(ActionMapping mapping, ActionForm  form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        request.getSession().invalidate();
+        
+        HttpSession session = request.getSession();
+        String sesId = session.getId();
+        session.invalidate();
+        log.info("Logged out from session: " + sesId);
         return mapping.findForward("success");
     }
 }
