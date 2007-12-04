1318a7828ce07176a6618ab86f3421e40f36f817
Chris Kramer <chriskramer@b3partners.nl>
2007-12-04 13:29:41 +0000

    Former-commit-id: 133956727e7314dab42f6f848197f3d9ed4fc876

---
+++ database/09 Account.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `kaartenbalie`.`acc_layerpricing`;
+CREATE TABLE  `kaartenbalie`.`acc_layerpricing` (
+  `lpr_id` int(11) NOT NULL auto_increment,
+  `lpr_LAYERID` int(11) default NULL,
+  `lpr_planType` int(11) default NULL,
+  `lpr_validFrom` datetime default NULL,
+  `lpr_validUntil` datetime default NULL,
+  `lpr_creationDate` datetime default NULL,
+  `lpr_unitPrice` decimal(9,2) default NULL,
+  PRIMARY KEY  (`lpr_id`),
+  KEY `FKE251E853EDD63704` (`lpr_LAYERID`),
+  CONSTRAINT `FKE251E853EDD63704` FOREIGN KEY (`lpr_LAYERID`) REFERENCES `layer` (`LAYERID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
