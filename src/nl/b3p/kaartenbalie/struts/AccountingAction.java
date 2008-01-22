e6b3d2369d8fa3bb16ca8554ebecbe5ee62155e6
Chris Kramer <chriskramer@b3partners.nl>
2008-01-22 15:12:23 +0000

    Accounting Fix
    Former-commit-id: 3f47ed966eb23fe59a8d1debca535c9805180578

--- src/nl/b3p/kaartenbalie/struts/AccountingAction.java
+++ src/nl/b3p/kaartenbalie/struts/AccountingAction.java
@@ -9,13 +9,18 @@
 
 package nl.b3p.kaartenbalie.struts;
 
+import java.util.List;
+import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import nl.b3p.commons.struts.ExtendedMethodProperties;
 import nl.b3p.kaartenbalie.core.server.Organization;
 import nl.b3p.kaartenbalie.core.server.User;
 import nl.b3p.kaartenbalie.core.server.accounting.AccountManager;
+import nl.b3p.kaartenbalie.core.server.accounting.entity.Transaction;
 import nl.b3p.kaartenbalie.core.server.accounting.entity.TransactionLayerUsage;
+import nl.b3p.kaartenbalie.core.server.accounting.entity.TransactionPaymentDeposit;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.validator.DynaValidatorForm;
@@ -26,21 +31,68 @@ import org.apache.struts.validator.DynaValidatorForm;
  */
 public class AccountingAction extends KaartenbalieCrudAction {
     
+    protected static final String TRANSACTION                        = "transaction";
+    protected Map getActionMethodPropertiesMap() {
+        Map map = super.getActionMethodPropertiesMap();
+        ExtendedMethodProperties crudProp = new ExtendedMethodProperties(TRANSACTION);
+        crudProp.setDefaultForwardName(SUCCESS);
+        crudProp.setDefaultMessageKey("beheer.accounting.transaction.succes");
+        crudProp.setAlternateForwardName(FAILURE);
+        crudProp.setAlternateMessageKey("beheer.accounting.transaction.succes");
+        map.put(TRANSACTION, crudProp);
+        return map;
+    }
+    
     public ActionForward unspecified(ActionMapping mapping, DynaValidatorForm dynaForm, HttpServletRequest request, HttpServletResponse response) throws Exception {
         prepareMethod(dynaForm, request, LIST, LIST);
         addDefaultMessage(mapping, request);
-        EntityManager em = getEntityManager();
-        User principalUser = (User) request.getUserPrincipal();
-        User user = (User) em.find(User.class, principalUser.getId());
-        Organization organization = user.getOrganization();
-        
+        return super.unspecified(mapping, dynaForm, request, response);
+    }
+    
+    public ActionForward transaction(ActionMapping mapping, DynaValidatorForm dynaForm, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String idString = request.getParameter("id");
+        request.setAttribute("id", idString);
+        if (idString != null && idString.length() > 0) {
+            Integer transactionId = new Integer(Integer.parseInt(idString));
+            Organization organization = getOrganization(request);
+            EntityManager em = getEntityManager();
+            Transaction transaction = (Transaction) em.createQuery(
+                    "FROM Transaction AS ta " +
+                    "WHERE ta.id = :transactionId AND ta.account.organization.id = :organizationId ")
+                    .setParameter("transactionId", transactionId)
+                    .setParameter("organizationId", organization.getId())
+                    .getSingleResult();
+            request.setAttribute("transaction",transaction);
+            
+            List layerUsageMutations = em.createQuery(
+                    "FROM LayerUsageMutation AS lum " +
+                    "WHERE lum.transactionLayerUsage.id = :transactionId")
+                    .setParameter("transactionId", transaction.getId())
+                    .getResultList();
+            request.setAttribute("LayerUsageMutations", layerUsageMutations );
+            
+            request.setAttribute("type",transaction.getClass().getSimpleName());
+        }
+        return super.unspecified(mapping, dynaForm, request, response);
+    }
+    
+    public void createLists(DynaValidatorForm form, HttpServletRequest request) throws Exception {
+        super.createLists(form, request);
+        Organization organization = getOrganization(request);
         AccountManager am = AccountManager.getAccountManager(organization.getId());
         request.setAttribute("balance", new Double(am.getBalance()));
         request.setAttribute("layerUsages", am.getTransactions(20, TransactionLayerUsage.class));
-        request.setAttribute("paymentDeposits", am.getTransactions(20, TransactionLayerUsage.class));
-        this.createLists(dynaForm, request);
-        return mapping.findForward(SUCCESS);
+        request.setAttribute("paymentDeposits", am.getTransactions(20, TransactionPaymentDeposit.class));
+        
     }
+    
+    private Organization getOrganization(HttpServletRequest request) {
+        EntityManager em = getEntityManager();
+        User principalUser = (User) request.getUserPrincipal();
+        User user = (User) em.find(User.class, principalUser.getId());
+        return user.getOrganization();
+    }
+    
     
     
     
