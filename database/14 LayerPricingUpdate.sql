2786545cb43e9e79194e0762c037192ec04d0d92
Chris Kramer <chriskramer@b3partners.nl>
2008-01-29 16:07:48 +0000

    Foutje in juiste database selectie...
    Former-commit-id: 22515a111d6272d7dd87ec7a5bb492f26ce9f828

--- database/14 LayerPricingUpdate.sql
+++ database/14 LayerPricingUpdate.sql
@@ -1,8 +1,8 @@
 ALTER TABLE `acc_layerpricing` ADD COLUMN `lpr_deletionDate` DATETIME AFTER `lpr_unitPrice`;
-ALTER TABLE `kaartenbalie_mnp`.`acc_layerpricing` ADD COLUMN `lpr_indexCount` INTEGER AFTER `lpr_deletionDate`;
+ALTER TABLE `acc_layerpricing` ADD COLUMN `lpr_indexCount` INTEGER AFTER `lpr_deletionDate`;
 
 
-DROP TABLE IF EXISTS `kaartenbalie_mnp`.`kb_uniquenumber`;
+DROP TABLE IF EXISTS `kb_uniquenumber`;
 CREATE TABLE  `kaartenbalie_mnp`.`kb_uniquenumber` (
   `unn_id` int(11) NOT NULL auto_increment,
   `unn_indexName` varchar(255) default NULL,
