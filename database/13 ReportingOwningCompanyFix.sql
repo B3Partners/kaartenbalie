2d475c90bfa2d6333dc4115645e5df4f0ad05408
Chris Kramer <chriskramer@b3partners.nl>
2008-01-29 15:53:49 +0000

    Former-commit-id: dbe049318e7df6962aa51b8bce9057378e0cd24a

--- database/13 ReportingOwningCompanyFix.sql
+++ database/13 ReportingOwningCompanyFix.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `kaartenbalie`.`rep_report` ADD COLUMN `rep_organizationId` INTEGER AFTER `rpd_org_id`;
+ALTER TABLE `rep_report` ADD COLUMN `rep_organizationId` INTEGER AFTER `rpd_org_id`;
