7a67dff9283c1bfff46dd550da4741bd9def2cb6
Matthijs Laan <matthijsln@gmail.com>
2008-02-25 14:33:54 +0000

    databasenaam verwijderd
    Former-commit-id: 3790d756d3d541e92ad9f09cb029f5309c902ca3

--- database/14 LayerPricingUpdate.sql
+++ database/14 LayerPricingUpdate.sql
@@ -1,9 +1,9 @@
-ALTER TABLE `acc_layerpricing` ADD COLUMN `lpr_deletionDate` DATETIME AFTER `lpr_unitPrice`;
+﻿ALTER TABLE `acc_layerpricing` ADD COLUMN `lpr_deletionDate` DATETIME AFTER `lpr_unitPrice`;
 ALTER TABLE `acc_layerpricing` ADD COLUMN `lpr_indexCount` INTEGER AFTER `lpr_deletionDate`;
 
 
 DROP TABLE IF EXISTS `kb_uniquenumber`;
-CREATE TABLE  `kaartenbalie_mnp`.`kb_uniquenumber` (
+CREATE TABLE  `kb_uniquenumber` (
   `unn_id` int(11) NOT NULL auto_increment,
   `unn_indexName` varchar(255) default NULL,
   `unn_indexCount` int(11) default NULL,
