73f6877b6ad409c8be468f3859ce5784eac05096
Matthijs Laan <matthijsln@gmail.com>
2009-04-03 13:11:52 +0000

    psql 8.1 compatible
    Former-commit-id: abaf9956d2dbd35b77dc2f35f54835bc3bfd5805

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -7,12 +7,11 @@ select setval('organization_id_seq', (select max(id) from organization));
 insert into account (id, credit_balance)
 	values (1, 0.0);
 	
-insert into roles (id, "role") values 
-    (1, 'beheerder'),
-    (2, 'organisatiebeheerder'),
-    (3, 'themabeheerder'),
-    (4, 'gebruiker'),
-    (5, 'demogebruiker'); 
+insert into roles (id, "role") values (1, 'beheerder');
+insert into roles (id, "role") values (2, 'organisatiebeheerder');
+insert into roles (id, "role") values (3, 'themabeheerder');
+insert into roles (id, "role") values (4, 'gebruiker');
+insert into roles (id, "role") values (5, 'demogebruiker');
     
 select setval('roles_id_seq', (select max(id) from roles));
 
@@ -21,8 +20,7 @@ insert into _user (id, organization, first_name, surname, email_address, usernam
 
 select setval('_user_id_seq', (select max(id) from _user));
 
-insert into _user_roles (_user, "role") values
-    (1,1),
-    (1,2),
-    (1,3),
-    (1,4);
+insert into _user_roles (_user, "role") values (1,1);
+insert into _user_roles (_user, "role") values (1,2);
+insert into _user_roles (_user, "role") values (1,3);
+insert into _user_roles (_user, "role") values (1,4);
