61d74cec64aaa68a4cca0f8541bcf1980ddc0419
Rachelle Scheijen <rachellescheijen@b3partners.nl>
2012-04-19 14:48:49 +0000

    TestKlasse KBImageToolTest voltooid en project prop. terug gezet zodat de DB werkt
    Former-commit-id: 9bc9ea8d66f0a30c2d73dc88c67efd4855ada2bf

--- test/general/KaartenbalieTestCase.java
+++ test/general/KaartenbalieTestCase.java
@@ -1,5 +1,7 @@
 package general;
 
+import nl.b3p.kaartenbalie.core.server.persistence.MyEMFDatabase;
+import nl.b3p.servletAPI.ConfigStub;
 import nl.b3p.testStreet.B3TestCase;
 
 /**
@@ -7,8 +9,17 @@ import nl.b3p.testStreet.B3TestCase;
  * @author rachelle
  */
 public class KaartenbalieTestCase extends B3TestCase {
+    protected MyEMFDatabase DB;
+    
     public KaartenbalieTestCase(String name){
         super(name);
+    }
+    
+    protected void setLocalUpClass() throws Exception {
+        ConfigStub config    = new ConfigStub();
+        DB                   = new MyEMFDatabase();
+        DB.init(config);
+        DB.createEntityManager(MyEMFDatabase.MAIN_EM);
     }
     
     protected nl.b3p.kaartenbalie.core.server.User generateUser(){
--- test/nl/b3p/kaartenbalie/service/KBImageToolTest.java
+++ test/nl/b3p/kaartenbalie/service/KBImageToolTest.java
@@ -2,11 +2,15 @@ package nl.b3p.kaartenbalie.service;
 
 import general.KaartenbalieTestCase;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import javax.imageio.IIOException;
+import nl.b3p.kaartenbalie.core.server.monitoring.DataMonitoring;
 import nl.b3p.kaartenbalie.core.server.monitoring.ServiceProviderRequest;
 import nl.b3p.kaartenbalie.service.requesthandler.DataWrapper;
 import nl.b3p.servletAPI.GetMethodStub;
 import nl.b3p.servletAPI.HttpServletRequestStub;
 import nl.b3p.servletAPI.HttpServletResponseStub;
+import nl.b3p.servletAPI.InputStreamStub;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,7 +36,11 @@ public class KBImageToolTest extends KaartenbalieTestCase {
         
         this.request        = new HttpServletRequestStub();
         this.response       = new HttpServletResponseStub();
+        
+        
         this.wrapper        = new DataWrapper(this.request,this.response);
+        this.wrapper.setRequestReporting(new DataMonitoring());
+        
         this.instance       = new KBImageTool();
     }
     
@@ -51,13 +59,12 @@ public class KBImageToolTest extends KaartenbalieTestCase {
      * Test of readImage method, of class KBImageTool.
      */
     @Test
-    public void testReadImage() throws Exception {
+    public void testReadImage(){
         GetMethodStub method                = new GetMethodStub();
         
         String mime                         = "bullshit";
         ServiceProviderRequest wmsRequest   = new ServiceProviderRequest();
-        BufferedImage expResult             = new BufferedImage(20,20,BufferedImage.TYPE_3BYTE_BGR);
-        
+                
         try {
             KBImageTool.readImage(method, mime, wmsRequest);
             
@@ -85,8 +92,21 @@ public class KBImageToolTest extends KaartenbalieTestCase {
         mime    = "image/jpeg";
         
         try {
+            /* Set stream */
+            InputStreamStub is = (InputStreamStub) method.getResponseBodyAsStream();
+            ArrayList<Byte> content = new ArrayList<Byte>();
+            byte c  = 10;
+            content.add(c);
+            content.add(c);
+            content.add(c);
+            is.setContent(content);
+            
             KBImageTool.readImage(method, mime, wmsRequest);
             
+            fail("Exception : not a valid JPEG. Should fail.");
+            assertTrue(false);
+        }
+        catch(IIOException e){
             assertTrue(true);
         }
         catch(Exception e){
@@ -99,14 +119,31 @@ public class KBImageToolTest extends KaartenbalieTestCase {
      * Test of writeImage method, of class KBImageTool.
      */
     @Test
-    public void testWriteImage() throws Exception {
-        System.out.println("writeImage");
-        BufferedImage[] images = null;
-        String mime = "";
-        DataWrapper dw = null;
-        //KBImageTool.writeImage(images, mime, dw);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+    public void testWriteImage(){
+        String mime                         = "bullshit";
+        BufferedImage[] images              = this.getImages();
+        
+        try {
+            KBImageTool.writeImage(images, mime, this.wrapper);
+            
+            fail("Mimetype should not be accepted");
+            
+            assertTrue(false);
+        }
+        catch(Exception e){
+            assertTrue(true);
+        }
+        
+        mime    = "image/jpeg";
+        try {
+            KBImageTool.writeImage(images, mime, this.wrapper);
+            
+            assertTrue(true);
+        }
+        catch(Exception e){
+            fail("Exception : "+e.getLocalizedMessage());
+            assertTrue(false);
+        }
     }
 
     /**
@@ -114,14 +151,25 @@ public class KBImageToolTest extends KaartenbalieTestCase {
      */
     @Test
     public void testCombineImages() {
-        System.out.println("combineImages");
-        BufferedImage[] images = null;
-        String mime = "";
-        BufferedImage expResult = null;
-        //BufferedImage result = KBImageTool.combineImages(images, mime);
-        //assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        try {
+            String mime                         = "image/png";
+            BufferedImage[] images              = this.getImages();
+            
+            KBImageTool.combineImages(images, mime);
+        }
+        catch(Exception e){
+            fail("Exception : "+e.getLocalizedMessage());
+        }
+        
+        try {
+            String mime                         = "image/jpeg";
+            BufferedImage[] images              = this.getImages();
+            
+            KBImageTool.combineImages(images, mime);
+        }
+        catch(Exception e){
+            fail("Exception : "+e.getLocalizedMessage());
+        }
     }
 
     /**
@@ -129,12 +177,20 @@ public class KBImageToolTest extends KaartenbalieTestCase {
      */
     @Test
     public void testGetMimeType() {
-        System.out.println("getMimeType");
-        String mime = "";
-        String expResult = "";
-        //String result = KBImageTool.getMimeType(mime);
-        //assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        String mime                         = "bullshit";
+        assertNull(KBImageTool.getMimeType(mime));
+        
+        mime                         = "image/png";
+        assertNotNull(KBImageTool.getMimeType(mime));        
+    }
+    
+    private BufferedImage[] getImages(){
+        BufferedImage expResult             = new BufferedImage(20,20,BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage expResult2            = new BufferedImage(20,20,BufferedImage.TYPE_INT_ARGB_PRE);        
+        BufferedImage[] images              = new BufferedImage[2];
+        images[0]   = expResult;
+        images[1]   = expResult2;
+        
+        return images;
     }
 }
