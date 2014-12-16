eac48548355e1c976dc78131391bf5a2f38e5714
Matthijs Laan <matthijsln@gmail.com>
2014-12-16 15:46:11 +0000

    melding bij loggen exception
    Former-commit-id: 864c17534a1f62f45f8cb3b8787c2cb79fe1ce55

--- src/nl/b3p/kaartenbalie/core/server/monitoring/DataMonitoring.java
+++ src/nl/b3p/kaartenbalie/core/server/monitoring/DataMonitoring.java
@@ -3,19 +3,19 @@
  * for authentication/authorization, pricing and usage reporting.
  *
  * Copyright 2006, 2007, 2008 B3Partners BV
- * 
+ *
  * This file is part of B3P Kaartenbalie.
- * 
+ *
  * B3P Kaartenbalie is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * B3P Kaartenbalie is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with B3P Kaartenbalie.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -120,7 +120,7 @@ public class DataMonitoring {
     }
 
     public void endClientRequest(String service, String operation, int bytesSentToUser, long totalResponseTime) {
-        
+
         if (!isEnableMonitoring()) {
             return;
         }
@@ -152,9 +152,9 @@ public class DataMonitoring {
             em.persist(clientRequest);
             em.flush();
         } catch (Exception e) {
-            log.error("", e);
+            log.error("Error logging request", e);
         }
-        
+
         clientRequest = null;
     }
 
